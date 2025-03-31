1c2237e11424f2f7b7b160e12394654f967f61dc
MaDoG <aliasgraphics@icloud.com>
2025-03-31 16:40:29 +0300

    Initial Commit

---
+++ CardWar/CardWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardWarApp.swift
+//  CardWar
+//
+//  Created by Denis Lachikhin on 31.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CardWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
